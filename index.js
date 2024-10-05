ab5d9903d2b55406fbfa6e572e00d4d802f40560
blazerzs <92708698+blazerzs@users.noreply.github.com>
2024-10-05 11:20:57 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// Export our request handler
 export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-    
-    // Construct the new URL to forward the request to discord.com
-    const newUrl = `https://discord.com${url.pathname}${url.search}`;
+  async fetch(request, env) {
+    console.log(`>>> NEW REQUEST INCOMING: ${request.url}`);
 
-    return fetch(newUrl, {
-      method: request.method,
-      headers: request.headers,
-      body: request.body
-    });
+    const rateLimiterId = env.RATE_LIMITER.idFromName("global_rate_limiter");
+    const rateLimiterObject = env.RATE_LIMITER.get(rateLimiterId);
+
+    return rateLimiterObject.fetch(request);
   }
-}
+};
